957d0dda4f8ff38f93d3370cfd95ce49f653f429
Anshuman Bhardwaj <anshu5074@gmail.com>
2023-01-02 15:20:41 +0530

    rename res to response

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.send("Rapyd example!");
+app.get("/", (request, response) => {
+  response.send("Rapyd example!");
 });
 
 function makeRequest(path, data, method) {
